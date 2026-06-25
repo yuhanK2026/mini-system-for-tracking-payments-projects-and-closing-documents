dfee0c4dc8fe6363779efed6df0bbc183786dc19
yanmei0825 <yanmei08252mail.ru>
2026-06-25 20:25:45 +0300

    Basic logic

--- app/Models/Act.php
+++ app/Models/Act.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Act extends Model
 {
-    //
+    public function getStatusAttribute()
+    {
+        if (!$this->is_sent && !$this->is_signed) {
+            return 'not_sent';
+        }
+
+        if ($this->is_sent && !$this->is_signed) {
+            return 'awaiting_signature';
+        }
+
+        if ($this->is_sent && $this->is_signed) {
+            return 'closed';
+        }
+
+        return 'attention_required';
+    }
 }
